fc0f9d9983ebd70e1f914cb85fcfd6978977c459
haikali3 <manfdvcL9@gmail.com>
2025-04-16 19:24:18 +0800

    feat: implement Home component with UI elements for user interaction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import { AppSidebar } from "./components/app-sidebar";
 import { SidebarProvider, SidebarInset } from "./components/ui/sidebar";
-import { Button } from "./components/ui/button";
-import { SmilePlus } from "lucide-react";
 import Header from "./components/header/header";
+import { Home } from "./pages/home";
 
 function App() {
   return (
@@ -12,37 +11,7 @@ function App() {
         <AppSidebar />
         <SidebarInset>
           <Header />
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-              <div>
-                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
-                <Button
-                  // onClick={generateSmiley}
-                  // disabled={loading}
-                  className="mt-4 w-full"
-                >
-                  <SmilePlus className="w-4 h-4" />
-                  {/* {loading ? "Generating..." : "Add Smiley Face"} */}
-                  Add Smiley Face
-                </Button>
-              </div>
-              <div>
-                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
-                <Button className="mt-4 w-full">
-                  <SmilePlus className="w-4 h-4" />
-                  Add Smiley Face
-                </Button>
-              </div>
-              <div>
-                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
-                <Button className="mt-4 w-full">
-                  <SmilePlus className="w-4 h-4" />
-                  Add Smiley Face
-                </Button>
-              </div>
-            </div>
-            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-          </div>
+          <Home />
         </SidebarInset>
       </SidebarProvider>
     </>
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { SmilePlus } from "lucide-react";
+
+export const Home = () => {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div>
+          <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+          <Button
+            // onClick={generateSmiley}
+            // disabled={loading}
+            className="mt-4 w-full"
+          >
+            <SmilePlus className="w-4 h-4" />
+            {/* {loading ? "Generating..." : "Add Smiley Face"} */}
+            Add Smiley Face
+          </Button>
+        </div>
+        <div>
+          <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+          <Button className="mt-4 w-full">
+            <SmilePlus className="w-4 h-4" />
+            Add Smiley Face
+          </Button>
+        </div>
+        <div>
+          <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+          <Button className="mt-4 w-full">
+            <SmilePlus className="w-4 h-4" />
+            Add Smiley Face
+          </Button>
+        </div>
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+    </div>
+  );
+};
