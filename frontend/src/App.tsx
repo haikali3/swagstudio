07acd6690ca254f3596fc6b0da7ef408a445cc51
haikali3 <manfdvcL9@gmail.com>
2025-04-16 19:15:32 +0800

    feat: refactor App component and add Header for improved layout and navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
 import "./App.css";
-import { Separator } from "@radix-ui/react-separator";
 import { AppSidebar } from "./components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbList,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  BreadcrumbPage,
-} from "./components/ui/breadcrumb";
-import {
-  SidebarProvider,
-  SidebarInset,
-  SidebarTrigger,
-} from "./components/ui/sidebar";
+import { SidebarProvider, SidebarInset } from "./components/ui/sidebar";
+import { Button } from "./components/ui/button";
+import { SmilePlus } from "lucide-react";
+import Header from "./components/header/header";
 
 function App() {
   return (
@@ -21,28 +11,35 @@ function App() {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">Products</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Supplier - HIT</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-          </header>
+          <Header />
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-              <div className="aspect-video rounded-xl bg-muted/50" />
-              <div className="aspect-video rounded-xl bg-muted/50" />
-              <div className="aspect-video rounded-xl bg-muted/50" />
+              <div>
+                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+                <Button
+                  // onClick={generateSmiley}
+                  // disabled={loading}
+                  className="mt-4 w-full"
+                >
+                  <SmilePlus className="w-4 h-4" />
+                  {/* {loading ? "Generating..." : "Add Smiley Face"} */}
+                  Add Smiley Face
+                </Button>
+              </div>
+              <div>
+                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+                <Button className="mt-4 w-full">
+                  <SmilePlus className="w-4 h-4" />
+                  Add Smiley Face
+                </Button>
+              </div>
+              <div>
+                <div className="aspect-video rounded-xl bg-muted/50 flex flex-col items-center justify-center"></div>
+                <Button className="mt-4 w-full">
+                  <SmilePlus className="w-4 h-4" />
+                  Add Smiley Face
+                </Button>
+              </div>
             </div>
             <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
           </div>
